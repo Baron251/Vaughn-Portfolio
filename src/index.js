8d27ae6ea325c3e81e5cf7c5d099cd4dc9ada42f
Baron <scorpiov2000@gmail.com>
2023-08-30 16:09:23 -0400

    swapped over to hashrouter from the previous commit

--- src/index.js
+++ src/index.js
@@ -4,16 +4,16 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { BrowserRouter } from 'react-router-dom';
-// import { HashRouter } from 'react-router-dom'
+// import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom'
 
 ReactDOM.render(
   <React.StrictMode>
-    {/* <HashRouter> */}
-    <BrowserRouter>
+    <HashRouter>
+    {/* <BrowserRouter> */}
     <App />
-    </BrowserRouter>
-    {/* </HashRouter> */}
+    {/* </BrowserRouter> */}
+    </HashRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
