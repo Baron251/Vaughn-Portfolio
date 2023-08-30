823eb7600c9470702f5a2dccacf4de69b3cab694
Baron <scorpiov2000@gmail.com>
2023-08-30 11:31:36 -0400

    Reversion of # on routes

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -43,22 +43,22 @@ export default function Navbar() {
 				<Col>
 					<Nav fill justified tabs>
 						<NavItem>
-							<NavLink href="#/">Home</NavLink>
+							<NavLink href="/">Home</NavLink>
 						</NavItem>
 						<NavItem>
-							<NavLink href="#/aboutme">About Me</NavLink>
+							<NavLink href="/aboutme">About Me</NavLink>
 						</NavItem>
 						<NavItem>
-							<NavLink href="#/projects">Projects</NavLink>
+							<NavLink href="/projects">Projects</NavLink>
 						</NavItem>
 						<NavItem>
-							<NavLink href="#/interests">Interests</NavLink>
+							<NavLink href="/interests">Interests</NavLink>
 						</NavItem>
 						<NavItem>
-							<NavLink href="#/work">Work History</NavLink>
+							<NavLink href="/work">Work History</NavLink>
 						</NavItem>
 						<NavItem>
-							<NavLink href="#/contact">Contact Me</NavLink>
+							<NavLink href="/contact">Contact Me</NavLink>
 						</NavItem>
 					</Nav>
 				</Col>
